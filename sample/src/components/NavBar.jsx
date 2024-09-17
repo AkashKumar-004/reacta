4f6e354b5dc40195d6c5854016b326303db0d300
Akashkumar S <s.akashkumar004@gmail.com>
2024-09-17 21:49:51 +0530

    Update NavBar.jsx

--- sample/src/components/NavBar.jsx
+++ sample/src/components/NavBar.jsx
@@ -1,8 +1,9 @@
-const Navbar=(ak)=>{
+const Navbar=({data})=>{
+    //const da=data.map((data1)=>{console.table(data1)})
+    console.log(data)
     return(
         <>
         <div className="bg-black">hi
-            {ak}
         </div>
         </>
     )
