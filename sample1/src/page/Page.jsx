7e0ddb72d4160cb1b43d6a7bfbf8adf6e3a1699b
Akashkumar S <s.akashkumar004@gmail.com>
2024-09-18 22:51:06 +0530

    Update Page.jsx

--- sample1/src/page/Page.jsx
+++ sample1/src/page/Page.jsx
@@ -4,20 +4,23 @@ const Page=()=>{
     return(
         <>
         <div className="h-screen w-screen ">
-           
-         <div className=""><NavBar/></div>
-            <div className="h-3/5 w-full flex flex-row">
-                <div className="w-3/5 h-full flex items-center justify-center"><img src={Cat}></img></div>
-                <div className="w-3/5 h-full flex justify-center items-center flex-col gap-4">
-                <div className=" h-[30%]">
-                    <div className="flex justify-center items-center">AKASH KUMAR</div>
-                </div>
-                <div className="h-[30%]">
-                    o
-                </div>
+            <div className="h-[80vh] w-[100%] flex flex-row bg-sky-300">
+                <div className="w-[40%] h-full flex items-center justify-center shadow-2xl"><img src={Cat}></img></div>
+                <div className="w-[60%] h-full flex gap-6 flex-col">
+                    <div className=" h-[50%] w-full flex justify-center items-center shadow-md">
+                            <div className="w-[80%] flex justify-center items-start flex-col">AKASH KUMAR S</div>
+                    </div>
+                    <div className=" h-[50%] flex items-center justify-center flex-col">
+                                <div className="w-[80%] h-[20%] flex ">
+                                    <div className="font-bold text-2xl">Discription</div>
+                                </div>
+                                <div className="w-[80%] h-[80%] flex justify-end ">
+                                    <div className="w-[80%]">hi</div>
+                                </div>
+                    </div>
                 </div>
             </div>
-            <div className="h-1/6"></div>   
+            <div className="h-[20vh]"></div>   
         </div>
         </>
     )
