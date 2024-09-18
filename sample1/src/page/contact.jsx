f633082589d8019c71a642dc93070743d004facf
Akashkumar S <s.akashkumar004@gmail.com>
2024-09-18 22:51:12 +0530

    Update contact.jsx

--- sample1/src/page/contact.jsx
+++ sample1/src/page/contact.jsx
@@ -1,11 +1,19 @@
+import { useState } from "react"
 import NavBar from "../Components/NavBar"
 
 function Contact() {
+  const[visible,setvisible]=useState(false)
+  const[data,setdata]=useState({
+    email:'',
+    password:''
+  })
   return (
-    <div>
-      <NavBar/>
-      hi
-    </div>
+    <>
+ 
+      <div className="h-[80vh] w-screen flex justify-center items-center -z-10">
+        <button  className="bg-red-500 p-3 text-white w-32 rounded-sm"> Delete </button>
+      </div>
+    </>
   )
 }
 export default Contact
