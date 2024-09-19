bb8a108943e77860485ed6ad937ea660e3e5f88b
Akashkumar S <s.akashkumar004@gmail.com>
2024-09-19 23:29:32 +0530

    Create App.jsx

---
+++ redux/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import NavBar from './components/NavBar'
+import Login from './components/login'
+const App=()=>{
+  return(
+    <>
+    <NavBar />
+    <Login />
+    
+    </>
+  )
+}
+export default App
