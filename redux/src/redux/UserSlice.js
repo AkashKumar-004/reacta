dc58001166662f698cd0a4988dec86dd0acfdb03
Akashkumar S <s.akashkumar004@gmail.com>
2024-09-19 23:29:46 +0530

    Create UserSlice.js

---
+++ redux/src/redux/UserSlice.js
@@ -0,0 +1,13 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const UserSlice= createSlice({
+    name: "sliceName",
+    initialState: {},
+    reducers: {
+        
+    }
+       
+})
+
+export const { invalidate } = sliceName.actions
+export default sliceName.reducer
