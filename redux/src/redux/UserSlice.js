22501e2f86e7bddf679053a5300c877214f03e42
Akashkumar S <s.akashkumar004@gmail.com>
2024-09-21 01:39:09 +0530

    Update UserSlice.js

--- redux/src/redux/UserSlice.js
+++ redux/src/redux/UserSlice.js
@@ -1,13 +1,21 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const UserSlice= createSlice({
-    name: "sliceName",
-    initialState: {},
+    name: "Username",
+    initialState: {
+        username:null
+    },
     reducers: {
-        
+        login:(state,action)=>{
+            state.username=action.payload
+        },
+        logout: (state) =>{
+            state.username=null
+        }
     }
        
 })
 
-export const { invalidate } = sliceName.actions
-export default sliceName.reducer
+export const { login,logout } =UserSlice.actions
+export const selectuser=(state)=>state.username.username
+export default UserSlice.reducer
