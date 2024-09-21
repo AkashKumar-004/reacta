2e7a5693c7cdedc0ddc87501362cd23cc11c7334
Akashkumar S <s.akashkumar004@gmail.com>
2024-09-21 23:07:54 +0530

    Update contact.jsx

--- sample1/src/page/contact.jsx
+++ sample1/src/page/contact.jsx
@@ -1,21 +1,17 @@
 import { useState } from "react"
 import NavBar from "../Components/NavBar"
 import { useSelector } from "react-redux"
-import { canceluser } from "../redux/UserSlice"
+import { canceluser, stateuser } from "../redux/UserSlice"
 
 function Contact() {
-  const[visible,setvisible]=useState(false)
-  const[data,setdata]=useState({
-    email:'',
-    password:''
-  })
-  const minus=useSelector(canceluser)
-  console.log(minus)
+
+  const name=useSelector(stateuser)
+  //console.log(name)
   return (
     <>
       <div className="h-[80vh] w-screen flex justify-center items-center -z-10">
         <button  className="bg-red-500 p-3 text-white w-32 rounded-sm"> Delete </button>
-      <div>{data.minus||"hi"}</div>
+      <div>{name?.name||"hi"}</div>
       </div>
     </>
   )
